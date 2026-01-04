aa19aa4488ae7abcc28b9a68c0b867e056171de4
moirhira <ighermohamed@gmail.com>
2026-01-04 19:48:51 +0100

    add cpp05/ex00

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,43 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+#include <iostream>
+
+
+class Bureaucrat {
+    private:
+        const std::string _name;
+        int _grade;
+    public:
+        Bureaucrat();
+        Bureaucrat(std::string const &name, int grade);
+        Bureaucrat(const Bureaucrat& other);
+        Bureaucrat& operator=(const Bureaucrat& other);
+        ~Bureaucrat();
+
+        std::string getName() const;
+        int getGrade() const;
+        void incrementGrade();
+        void decrementGrade();
+
+        class GradeTooHighException : public std::exception {
+            public:
+                virtual const char* what() const throw();
+        };
+
+        class GradeTooLowException : public std::exception {
+            public:
+                virtual const char* what() const throw();
+        };
+
+};
+
+
+std::ostream& operator<<(std::ostream &out, const Bureaucrat& obj);
+
+
+#endif
---
+++ cpp05/ex00/main.cpp
@@ -0,0 +1,18 @@
+#include "Bureaucrat.hpp"
+
+
+int main() {
+    try
+    {
+        Bureaucrat b;
+        std::cout << b << std::endl;
+        b.incrementGrade();
+                std::cout << b << std::endl;
+
+    }
+    catch (std::exception & e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    return 0;
+}
