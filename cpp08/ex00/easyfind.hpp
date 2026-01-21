28ba3b0eb710f167bc0ce85b1be0ab35516fde59
moirhira <ighermohamed@gmail.com>
2026-01-21 17:19:33 +0100

    steup stracture  cpp08/ex00

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <exception>
+#include <algorithm>
+
+class NotFoundException : public std::exception {
+
+    const char * what() const throw ();
+};
+
+const char * NotFoundException::what() const throw() {
+    return "The number provided not found!";
+}
+
+template <typename T>
+typename T::iterator easyfind(T& ctnr, int n)
+{
+    typename T::iterator it = std::find(ctnr.begin(), ctnr.end(), n);
+
+    if (it == ctnr.end())
+        throw NotFoundException();
+
+    return it;
+}
