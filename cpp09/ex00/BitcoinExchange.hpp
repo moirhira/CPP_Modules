f18955daafde8a87068e050989278841ecd70875
moirhira <ighermohamed@gmail.com>
2026-01-31 21:54:26 +0100

    make the main class

---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,17 @@
+#include <map>
+#include <string>
+
+
+class BitcoinExchange {
+    private:
+        std::map<std::string, float> _database;
+    public:
+    BitcoinExchange(const std::map<std::string, float> & data);
+    BitcoinExchange(const BitcoinExchange & other);
+    BitcoinExchange operator=(const BitcoinExchange & other);
+    ~BitcoinExchange();
+
+    void laodDatabase(const std::string& filename);
+    void pocessInput(const std::string& filename);
+
+};
