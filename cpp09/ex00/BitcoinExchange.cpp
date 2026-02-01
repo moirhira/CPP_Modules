2a70c2124553995d4ddd4bfb8fea1fc10b3f7aab
moirhira <ighermohamed@gmail.com>
2026-02-01 22:05:28 +0100

    start proccessing the input in the file

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -1,5 +1,6 @@
 #include "BitcoinExchange.hpp"
 #include <cstdlib> 
+#include <algorithm>
 
 BitcoinExchange::BitcoinExchange() {
     this->loadDatabase("data.csv");
@@ -32,6 +33,44 @@ void BitcoinExchange::loadDatabase(const std::string& filename) {
     }
     datafile.close();
 }
+
+std::string trim(const std::string& str) {
+    size_t start = 0;
+    size_t end = str.size();
+
+    while (start < end && std::isspace(static_cast<unsigned char>(str[start])))
+        start++;
+
+    while (end > start && std::isspace(static_cast<unsigned char>(str[end - 1])))
+        end--;
+
+    return str.substr(start, end - start);
+}
+
 void BitcoinExchange::pocessInput(const std::string& filename) {
-    (void) filename;
+    std::ifstream inputFile(filename.c_str());
+    if (!inputFile.is_open()){
+        throw std::runtime_error("Error: at opening the file.");
+    }
+    std::string line;
+    std::getline(inputFile, line);
+    while (std::getline(inputFile, line))
+    {
+        size_t pipe_v = line.find('|');
+
+        if (pipe_v == std::string::npos)
+            throw std::runtime_error("Error: bad input.");
+        
+        std::string date_v = trim(line.substr(0, pipe_v));
+        std::string value_v = trim(line.substr(pipe_v + 1));
+        
+
+        if (std::count(date_v.begin(), date_v.end(), '-') != 2)
+            throw std::runtime_error("Error: bad inppput.");
+        
+        std::cout << date_v <<  " => " << value_v << " = " << std::endl;
+
+        
+        
+    }
 }
