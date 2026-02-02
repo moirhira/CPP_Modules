7650440a3a0b15f0873633b957db0a1bec91e62d
moirhira <ighermohamed@gmail.com>
2026-02-02 15:29:01 +0100

    finish the cpp09/ex00

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -1,14 +1,16 @@
 #include "BitcoinExchange.hpp"
-#include <cstdlib> 
+#include <cstdlib>
 #include <algorithm>
 
-BitcoinExchange::BitcoinExchange() {
+BitcoinExchange::BitcoinExchange()
+{
     this->loadDatabase("data.csv");
 }
 
-BitcoinExchange::BitcoinExchange(const BitcoinExchange & other) : _database(other._database) {}
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &other) : _database(other._database) {}
 
-BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange & other) {
+BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &other)
+{
     if (this != &other)
         this->_database = other._database;
     return *this;
@@ -16,14 +18,20 @@ BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange & other) {
 
 BitcoinExchange::~BitcoinExchange() {}
 
-void BitcoinExchange::loadDatabase(const std::string& filename) {
+
+
+
+void BitcoinExchange::loadDatabase(const std::string &filename)
+{
     std::ifstream datafile(filename.c_str());
-    if (!datafile.is_open()) {
+    if (!datafile.is_open())
+    {
         throw std::runtime_error("Error: at opening the file.");
     }
     std::string line;
     std::getline(datafile, line);
-    while (std::getline(datafile, line)) {
+    while (std::getline(datafile, line))
+    {
         size_t coma = line.find(',');
 
         std::string date_v = line.substr(0, coma);
@@ -34,7 +42,8 @@ void BitcoinExchange::loadDatabase(const std::string& filename) {
     datafile.close();
 }
 
-std::string trim(const std::string& str) {
+std::string trim(const std::string &str)
+{
     size_t start = 0;
     size_t end = str.size();
 
@@ -47,24 +56,45 @@ std::string trim(const std::string& str) {
     return str.substr(start, end - start);
 }
 
-bool isValidDate(const std::string& date)
+bool isValidDate(const std::string &date)
 {
     if (date.length() != 10 || date[4] != '-' || date[7] != '-')
         return false;
-    
 
     int year_v = std::atoi(date.substr(0, 4).c_str());
     int month_v = std::atoi(date.substr(5, 2).c_str());
     int day_v = std::atoi(date.substr(8, 2).c_str());
 
+    if ((month_v == 4 || month_v == 6 || month_v == 9 || month_v == 11) && day_v > 30)
+        return false;
+    if (month_v == 2 && day_v > 29)
+        return false; 
     if (year_v < 2009 || month_v < 1 || month_v > 12 || day_v < 1 || day_v > 31)
         return false;
 
     return true;
 }
-void BitcoinExchange::pocessInput(const std::string& filename) {
+
+float getTheRate(const std::string& date, const std::map<std::string, float>& data)
+{
+    std::map<std::string, float>::const_iterator it = data.find(date);
+    if (it != data.end())
+        return it->second;
+    
+    it = data.lower_bound(date);
+    if (it != data.begin())
+        return (--it)->second;
+    
+    return 0.0f;
+}
+
+
+
+void BitcoinExchange::pocessInput(const std::string &filename)
+{
     std::ifstream inputFile(filename.c_str());
-    if (!inputFile.is_open()){
+    if (!inputFile.is_open())
+    {
         throw std::runtime_error("Error: at opening the file.");
     }
     std::string line;
@@ -80,45 +110,38 @@ void BitcoinExchange::pocessInput(const std::string& filename) {
             std::cerr << "Error: bad input => " << line << std::endl;
             continue;
         }
-        
+
         std::string date_v = trim(line.substr(0, pipe_v));
         std::string value_v = trim(line.substr(pipe_v + 1));
-        
 
-        try {
-            if (!isValidDate(date_v))
-            {
-                std::cerr << "Error: bad input => " << date_v << std::endl;
-                continue;  
-            }
-        }
-        catch (...)
+        if (!isValidDate(date_v))
         {
             std::cerr << "Error: bad input => " << date_v << std::endl;
-            continue; 
+            continue;
         }
 
-  
+        char *end;
         float value;
-        try {
-            value = std::strtof(value_v.c_str(), NULL);
-        }
-        catch (...) {
-            std::cout << "Error: bad input => " << value_v << std::endl;
+        value = std::strtof(value_v.c_str(), &end);
+        if (value_v.empty() || *end != '\0')
+        {
+            std::cerr << "Error: bad input => " << value_v << std::endl;
             continue;
         }
         
-        if (value < 0){
-            std::cout << "Error: not a positive number." << std::endl; 
+        if (value < 0)
+        {
+            std::cout << "Error: not a positive number." << std::endl;
             continue;
         }
 
-        if (value > 1000){
+        if (value > 1000)
+        {
             std::cout << "Error: too large a number." << std::endl;
             continue;
-        }    
-        
-        
-        std::cout << date_v <<  " => " << value_v << " = " << std::endl;
+        }
+
+        float rate = getTheRate(date_v, _database);
+        std::cout << date_v << " => " << value_v << " = " << (value * rate) << std::endl;
     }
 }
