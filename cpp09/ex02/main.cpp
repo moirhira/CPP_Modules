83e795df3d6dc8e89dcd0958da29801569859edd
moirhira <ighermohamed@gmail.com>
2026-02-03 10:30:32 +0100

    Makefile cpp09/ex02

---
+++ cpp09/ex02/main.cpp
@@ -0,0 +1,14 @@
+#include "PmergeMe.hpp"
+
+
+
+int main(int ac, char **av)
+{
+    if (ac <= 2)
+    {
+        std::cerr << "Error: No arguments provided." << std::endl; 
+        return 1;
+    }
+    PmergeMe sorter;
+    sorter.sort(ac - 1, av);
+}
