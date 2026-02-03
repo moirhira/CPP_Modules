165127987eea4224dc8c5580f8565136814ebbe4
moirhira <ighermohamed@gmail.com>
2026-02-03 10:44:08 +0100

    parse the input number cpp09/ex02

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -1,6 +1,7 @@
 #include "PmergeMe.hpp"
 #include <cstdlib>
 #include <cstring>
+#include <climits>
 
 PmergeMe::PmergeMe() {}
 
@@ -20,3 +21,29 @@ PmergeMe::PmergeMe(const PmergeMe& other)
     : _vec(other._vec), _deq(other._deq) {}
 
 PmergeMe::~PmergeMe() {}
+
+
+
+void PmergeMe::sort(int len, char **argv) {
+
+    for (int i = 1; i < len; i++)
+    {
+        std::string arg = argv[i];
+
+        if (arg.find_first_not_of("012345679") != std::string::npos)
+        {
+            std::cerr << "Error: Invalid character found." << std::endl;
+            return;
+        }
+
+        long value = std::atol(argv[i]);
+        if (value < 0 || value > INT_MAX)
+        {
+            std::cerr << "Error: Number out of integer range." << std::endl;
+            return;
+        }
+        _vec.push_back(static_cast<int>(value));
+    }
+
+    
+}
